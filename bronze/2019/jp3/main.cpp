d71b5edbed6cdb9adca0efbcb501f202bc350473
squirrelcoding <rosilescarlos28@gmail.com>
2025-02-11 23:31:00 -0600

    wdefgtry

--- bronze/2019/jp3/main.cpp
+++ bronze/2019/jp3/main.cpp
@@ -42,15 +42,77 @@ int main() {
     animals.insert({name, characteristics});
   }
 
-  unordered_map<string, unordered_set<string>> animal_to_characteristic;
-  unordered_map<string, unordered_set<string>> characteristic_to_animal;
+
+  unordered_map<string, unordered_set<animal, animal_hash>> characteristic_to_animal;
   
   // Construct the stuff.
-  for (auto animal : animals) {
-    if (animal_to_characteristic.find(animal.name) == animal_to_characteristic.end()) {
+  for (const auto& animal : animals) {
+    for (const string& characteristic : animal.characteristics) {
+      characteristic_to_animal[characteristic].insert(animal);
     }
   }
+  /**/
+  /*cout << " === Characteristics === " << endl;*/
+  /*for (const auto& c: characteristic_to_animal) {*/
+  /*  cout << c.first << ": ";*/
+  /*  for (const auto& anim : c.second) {*/
+  /*    cout << anim.name << " ";*/
+  /*  }*/
+  /*  cout << endl;*/
+  /*}*/
 
+  // Ok so we want to start a for loop where we select the characteristic with the highest number of animals in it.
+  // In the animals set, we want to remove all of the animals in the current characteristic. While the set of animals
+  // is greater than 1, we keep incrementing a counter.
+
+  int counter = 0;
+  while (animals.size() > 1) {
+    // Find the characteristic with the most amount of animals in it.
+    string most_common_characteristic;
+    int max_amount = 0;
+    for (const auto& keypair : characteristic_to_animal) {
+      if (keypair.second.size() > max_amount) {
+        most_common_characteristic = keypair.first;
+        max_amount = keypair.second.size();
+      }
+    }
+    /*cout << "The most common characteristic is " << most_common_characteristic << endl;*/
+
+
+    vector<animal> to_be_deleted;
+
+    for (const auto& animal : animals) {
+
+      // Check if animal does NOT have the most common characteristic. In which case we delete it.
+      if (find(
+            animal.characteristics.begin(), 
+            animal.characteristics.end(), 
+            most_common_characteristic
+          ) == animal.characteristics.end()) {
+        
+        // Go to each of the animal's other characteristics and delete it from there.
+        for (const auto& other_char : animal.characteristics) {
+          characteristic_to_animal[other_char].erase(animal);
+        }
+
+        // Remove the animal from the main set.
+        to_be_deleted.push_back(animal);
+      } 
+        
+      
+    }
+
+    for (auto animal : to_be_deleted) {
+      animals.erase(animal);
+    }
+
+    // Delete the most common characteristic.
+    characteristic_to_animal.erase(most_common_characteristic);
+
+    counter++;
+  }
+
+  cout << counter << endl;
 
   return 0;
 }
