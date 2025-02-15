e435c4fbeba88241ed053393e9249a820cc61289
squirrelcoding <rosilescarlos28@gmail.com>
2025-02-14 23:06:27 -0600

    wsdefr

--- bronze/2018/jp2/main.cpp
+++ bronze/2018/jp2/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
 #include <algorithm> // Include this for the max function
-
 
 using namespace std;
 
@@ -10,37 +9,53 @@ typedef pair<int, int> shift;
 
 int main() {
   freopen("lifeguards.in", "r", stdin);
+  freopen("lifeguards.out", "w", stdout);
 
   int n;
   cin >> n;
 
   vector<shift> shifts(n);
 
+  int latest_time = 0;
+
   for (int i = 0; i < n; ++i) {
     int a; int b;
     cin >> a >> b;
+    latest_time = max(latest_time, max(a, b));
     shifts[i] = make_pair(a, b);
   }
 
-  unordered_set<int> times;
 
-  for (auto shift : shifts) {
-    for (int i = shift.first; i <= shift.second; ++i) {
-      times.insert(i);
-    }
+  unordered_map<int, int> times;
+  for (int i = 0; i < latest_time; ++i) {
+    times[i] = 0;
   }
 
+  for (auto shift : shifts) {
+    for (int i = shift.first; i < shift.second; ++i) {
+      times[i]++;
+    }
+  }
+  
   int max_time = 0;
   for (auto shift : shifts) {
-    // Remove the times in shift from times
-    for (int i = shift.first; i <= shift.second; ++i) {
-      times.erase(i);
-    }
-    max_time = max(max_time, static_cast<int>(times.size()));
+    int s = 0;
 
-    for (int i = shift.first; i <= shift.second; ++i) {
-      times.insert(i);
+    // Remove the shift.
+    for (int i = shift.first; i < shift.second; ++i) {
+      times[i]--;
     }
+    for (int i = 0; i < latest_time; ++i) {
+      if (times[i] > 0) {
+        s += 1;
+      }
+    }
+    for (int i = shift.first; i < shift.second; ++i) {
+      times[i]++;
+    }
+
+    // Count the number of times there is a nonzero entry.
+    max_time = max(max_time, s);
   }
 
   cout << max_time << endl;
