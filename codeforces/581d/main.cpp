894201f12e5942dbf4260c34a3484736884e9100
squirrelcoding <rosilescarlos28@gmail.com>
2025-02-27 22:45:26 -0600

    dwefrtg

--- codeforces/581d/main.cpp
+++ codeforces/581d/main.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 typedef pair<int, int> box;
 
+
+void print(int b_x, int c_x, int n, int y) {
+  for (int i = 0; i < y; ++i) {
+      // Print the first 
+      for (int j = 0; j < b_x; ++j) {
+        cout << "B";
+      }
+      for (int j = 0; j < c_x; ++j) {
+        cout << "C";
+      }
+      cout << endl;
+    }
+    for (int j = 0; j < n - y; ++j) {
+      for (int k = 0; k < n; ++k) cout << "A";
+      cout << endl;
+    } 
+}
+
 int main() {
   vector<int> sizes;
 
@@ -18,6 +36,8 @@ int main() {
       max_box = i / 2;
     }
   }
+
+  cout << n << endl;
 
   // Sort the boxes by having the larger coordinate always be first.
   for (int i = 0; i < 6; i += 2) {
@@ -35,15 +55,10 @@ int main() {
     }
   }
 
-
-  for (auto b : boxes) {
-    cout << "Smaller box: " << "(" << b.first << ", " << b.second << ")" << endl;
-  }
-
   // If stacking all three on top of each other doesn't work, try the other way.
   // Stacking will only work if all three have some coordinate that is all the same
   // So we check for that here.
-  if (sizes[0] == sizes[2] && sizes[2] == sizes[4]) {
+  if (sizes[0] == sizes[2] && sizes[2] == sizes[4] && sizes[1] + sizes[3] + sizes[5] == n) {
     for (int i = 0; i < sizes[1]; ++i) {
       for (int j = 0; j < n; j++) cout << "A";
       cout << endl;
@@ -59,20 +74,24 @@ int main() {
     return 0;
   }
 
+  int y = n - min(sizes[2*max_box], sizes[2*max_box+1]);
+
   // Attempt to fit the other two smaller boxes
-  if (boxes[0].first + boxes[1].first == n) {
-    
+  if (boxes[0].first + boxes[1].first == n && boxes[0].second == y && boxes[1].second == y) {
+    print(boxes[0].first, boxes[1].first, n, y);
   } 
-  else if (boxes[0].first + boxes[1].second == n) {
-
+  else if (boxes[0].first + boxes[1].second == n && (boxes[0].second == y) && (boxes[1].first) == y) {
+    print(boxes[0].first, boxes[1].second, n, y);
   } 
-  else if (boxes[0].second + boxes[1].first == n) {
-
+  else if (boxes[0].second + boxes[1].first == n && boxes[0].first == y && boxes[1].second == y) {
+    print(boxes[0].second, boxes[1].first, n, y);
   } 
-  else if (boxes[0].second + boxes[1].second == n) {
+  else if (boxes[0].second + boxes[1].second == n && boxes[1].first + boxes[1].first == y) {
+    print(boxes[0].second, boxes[1].second, n, y);
+  }
+  else {
+    cout << "-1" << endl;  
+  }
 
-  } 
-
-  cout << "-1" << endl;  
   return 0;
 }
