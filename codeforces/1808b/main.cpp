5d97e2e4bff7bc508771ff7228ed2728610aae8e
squirrelcoding <rosilescarlos28@gmail.com>
2025-03-01 19:50:13 -0600

    edfrg

--- codeforces/1808b/main.cpp
+++ codeforces/1808b/main.cpp
@@ -4,56 +4,60 @@ using namespace std;
 
 #define all(x) x.begin(), x.end()
 
-int nxt() {
-  int x;
-  cin >> x;
-  return x;
+long long nxt() {
+    long long x;
+    cin >> x;
+    return x;
 }
 
-int solve(vector<vector<int>>& cards, int n, int m) {
-
-  int s = 0;
-  for (int card1 = 0; card1 < n; ++card1) {
-    for (int card2 = card1; card2 < n; ++card2) {
-      for (int i = 0; i < m; ++i) {
-        s += abs(cards[card1][i] - cards[card2][i]);
-      }
+long long solve(vector<vector<long long>>& cards, long long n, long long m) {
+    long long s = 0;
+    
+    // Iterate over columns
+    for (long long column = 0; column < m; ++column) {
+        vector<long long> col_values(n);
+        
+        // Extract values from each row for this column
+        for (long long row = 0; row < n; ++row) {
+            col_values[row] = cards[row][column];
+        }
+        
+        // Sort column values
+        sort(all(col_values));
+        
+        // Apply formula
+        for (long long i = 1; i <= n; ++i) {
+            s += col_values[i - 1] * (2 * i - n - 1);
+        }
     }
-  }
-
-  return s;
+    return s;
 }
 
-vector<vector<int>> get_cards() {
-  int n = nxt();
-  int m = nxt();
-  vector<vector<int>> cards(n);
+vector<vector<long long>> get_cards() {
+    long long n = nxt();
+    long long m = nxt();
+    
+    // Store cards row-wise
+    vector<vector<long long>> cards(n, vector<long long>(m, 0));
 
-  for (int i = 0; i < n; i++) {
-    vector<int> vec(m);
-    for (int j = 0; j < m; j++) {
-      vec[j] = nxt();
+    // Read input correctly
+    for (long long i = 0; i < n; i++) {
+        for (long long j = 0; j < m; j++) {
+            cards[i][j] = nxt();
+        }
     }
-    cards[i] = vec;
-  }
-  return cards;
+    
+    return cards;
 }
-
 
 int main() {
-  int t = nxt();
+    long long t = nxt();
+    
+    for (long long i = 0; i < t; ++i) {
+        vector<vector<long long>> cards = get_cards();
+        cout << solve(cards, cards.size(), cards[0].size()) << endl;
+    }
 
-  vector<vector<vector<int>>> cases(t);
-
-  vector<int> results(t);
-
-  for (int i = 0; i < t; ++i) {
-    cases[i] = get_cards();
-  }
-
-  for (auto x : cases) {
-    cout << solve(x, x.size(), x[0].size()) << endl;
-  }
-
-  return 0;
+    return 0;
 }
+
---
+++ codeforces/863b/main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+int nxt() {
+  int n;
+  cin >> n;
+  return n;
+}
+
+int main() {
+  // Do a stars and bars search that's like O(n^2) time complexity.
+  return 0;
+}
