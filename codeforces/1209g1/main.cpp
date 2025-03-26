3207471f1c3eb8e1f1d938f615212abf48169ea6
squirrelcoding <rosilescarlos28@gmail.com>
2025-03-25 23:21:14 -0500

    DEfrg

--- codeforces/1209g1/main.cpp
+++ codeforces/1209g1/main.cpp
@@ -9,7 +9,7 @@ int nxt() {
 }
 
 int main() {
-  freopen("input", "r", stdin);
+  /*freopen("input", "r", stdin);*/
   int n = nxt();
   cin.ignore(2); // since q=0 in all inputs
                  //
@@ -18,7 +18,6 @@ int main() {
 
 
   unordered_map<int, pair<int, int>> intervals;
-  unordered_map<int, int> frequencies;
   unordered_map<int, int> block_names;
 
   vector<pair<int, int>> final_intervals;
@@ -29,7 +28,6 @@ int main() {
     } else {
       intervals[nums[i]].second = i;
     }
-    frequencies[nums[i]]++;
   }
 
 
@@ -41,36 +39,46 @@ int main() {
 
 
   while (p1 < n) {
-    while (p2 != end_of_previous_block) {
-      cout << "p1 = " << p1 << ", p2 = " << p2 << endl;
+    while (p2 > end_of_previous_block - 1) {
+      // break
       auto interval = intervals[nums[p2]];
       if (interval.second > p1) {
-        // Update the "name" of the block to be the most frequent one
-        if (frequencies[nums[interval.second]] > frequencies[nums[p1]]) {
-          block_names[p1] = nums[interval.second];
-        }
-        
+       
         p1 = interval.second;
         p2 = p1;
-        break;
       }
       --p2;
     }
 
-
     final_intervals.push_back({end_of_previous_block, p1});
-    block_names[p1] = nums[p1];
-    end_of_previous_block = p1;
-
+    end_of_previous_block = p1 + 1;
     
     // Start a new block
     p1 += 1;
     p2 = p1;
   }
 
+  int grand_sum = 0;
+
+  // Form the block names for each entry
+  unordered_map<int, int> frequencies;
+
   for (const auto& entry : final_intervals) {
-    cout << "(" << entry.first << ", " << entry.second<< ")" << endl;
+    for (int i = entry.first; i <= entry.second; ++i) {
+      frequencies[nums[i]]++; 
+    }
+    auto maxIt = std::max_element(all(frequencies),
+        [](const auto& p1, const auto& p2) {
+            return p1.second < p2.second;
+        });
+    
+    grand_sum += (entry.second - entry.first + 1) - maxIt->second;
+
+    // Get the most common one
+    frequencies.clear();
   }
+
+  cout << grand_sum << endl;
 
   return 0;
 }
