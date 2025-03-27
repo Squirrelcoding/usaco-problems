53b5f751f3c5ae09e792996215fae5fb83690010
squirrelcoding <rosilescarlos28@gmail.com>
2025-03-27 00:20:33 -0500

    edwfrtg

--- bronze/2018/op2/main.cpp
+++ bronze/2018/op2/main.cpp
@@ -10,7 +10,7 @@ int nxt() {
 
 int main() {
   freopen("milkorder.in", "r", stdin);
-  /*freopen("milkorder.out", "w", stdout);*/
+  freopen("milkorder.out", "w", stdout);
   int n = nxt();
   int m = nxt();
   int k = nxt();
@@ -23,33 +23,74 @@ int main() {
 
   unordered_map<int, int> demands;
   vector<int> res(n + 1);
+  vector<int> res_copy(n + 1);
   for (int i = 0; i < k; ++i) {
     int a = 0; int b = 0;
     cin >> a; cin >> b;
     demands[a] = b;
     res[b] = a;
+    res_copy[b] = a;
   }
 
+  set<int> instances;
   for (int candidate = 1; candidate <= n; ++candidate) {
-
+    if (res[candidate] != 0) continue;
     // Set in the candidate
     res[candidate] = 1;
     // Since the demands are already filled in we just have to fill in the
     // social hierarchy
     int j = 1;
     int count = 0;
-    while (count < k) {
-      if (res[j] == 0) {
+    while (count <= k) {
+      if (res[j] == 0 || (res[j] == social_h[count] && demands.count(res[j]) != 0)) {
         res[j] = social_h[count];
         ++count;
       }
       ++j;
     }
+    /*cout << "candidate = " << candidate << endl;*/
+    /*for (int x : res) cout << x << " ";*/
+    /*cout << endl;*/
 
     // Check if it's consistent by checking:
-    // 1. The social hierarchy is satisfied
-    // 2. There is only one instance of everything
+    // 1. There is only one instance of everything
+    // 2. The social hierarchy is satisfied
+    bool cont = false;
+    for (int num : res) {
+      if (num == 0) continue;
+      if (instances.count(num) != 0) {
+        cont = true;
+        break;
+      }
+      instances.insert(num);
+    }
 
+    /*cout << "64" << endl;*/
+    if (cont) {
+      res = res_copy;
+      instances = {};
+      continue;
+    }
+
+    /*cout << "71" << endl;*/
+    int social_ptr = 0;
+    for (int i = 1; i <= n; ++i) {
+      if (res[i] == social_h[social_ptr]) {
+        social_ptr++;
+      }
+    }
+
+    if (social_ptr < k) {
+      instances = {};
+      res = res_copy;
+      continue;
+    }
+
+    // It has been found!!
+    auto it = find(all(res), 1);
+    int index = distance(res.begin(), it);
+    cout << index << endl;
+    break;
   }
 
 
