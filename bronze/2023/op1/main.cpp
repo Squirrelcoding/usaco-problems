8374f73b7242cde21398390d30874285bf60ee9e
squirrelcoding <rosilescarlos28@gmail.com>
2025-05-15 23:42:56 -0500

    back

---
+++ bronze/2023/op1/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+int nxt() {
+  int n; cin >> n; return n;
+}
+
+int main() {
+  freopen("input", "r", stdin);
+  int n = nxt();
+  string s;
+  cin >> s;
+}
