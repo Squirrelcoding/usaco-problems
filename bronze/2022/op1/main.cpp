f304a9bc4908e3ca0add7797a87b2d8ff6b72f5f
squirrelcoding <rosilescarlos28@gmail.com>
2025-06-21 11:13:11 -0500

    edfrgt

--- bronze/2022/op1/main.cpp
+++ bronze/2022/op1/main.cpp
@@ -9,68 +9,27 @@ int nxt() {
   int n; cin >> n; return n;
 }
 
-enum GType {
-  TWO_G,
-  EVEN_G,
-  ODD_G,
-  NO_G
-};
-
 int main() {
   /*freopen("input", "r", stdin);*/
   int n = nxt();
   string input;
   cin >> input;
 
-  vector<GType> data; 
+  vector<int> data; 
 
   for (int i = 0; i < n; i += 2) {
     char first_char = input[i];
     char second_char = input[i + 1];
-    if (first_char == second_char && first_char == 'H') {
-      data.push_back(GType::NO_G);
-    } else if (first_char == second_char && first_char == 'G') {
-      data.push_back(GType::TWO_G);
-    } else if (first_char == 'G' && second_char == 'H') {
-      data.push_back(GType::ODD_G);
-    } else {
-      data.push_back(GType::EVEN_G);
+    if (first_char == 'G' && second_char == 'H') {
+      data.push_back(1);
+    } else if (first_char == 'H' && second_char == 'G') {
+      data.push_back(0);
     }
   }
 
-
-
   int step_counter = 0;
   for (int i = data.size() - 1; i >= 0; --i) {
-    // Check status of current element 
-    GType current = data[i];
-    if (current == GType::ODD_G) {
-      ++step_counter;
-      /*cout << "At i = " << i << " we are flipping." << endl;*/
-      // Reverse all of the cows up to and including i
-      for (int j = 0; j <= i; ++j) {
-        switch (data[j]) {
-          case GType::ODD_G:
-            data[j] = GType::EVEN_G;
-            break;
-          case GType::EVEN_G:
-            data[j] = GType::ODD_G;
-            break;
-          default:
-            continue;
-        }
-      }
-      /**/
-      /*for (GType g : data) {*/
-      /*  cout << g << endl;*/
-      /*}*/
-      /**/
-      /*cout << endl;*/
-      /*cout << endl;*/
-      /*cout << endl;*/
-      /*cout << endl;*/
-      /*cout << endl;*/
-    } 
+    if ((data[i] + step_counter) % 2 == 1) ++step_counter;
   }
 
   cout << step_counter << endl;
