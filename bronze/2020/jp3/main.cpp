e768abfe5f1f3b104e18e242f169b9c885202cba
squirrelcoding <rosilescarlos28@gmail.com>
2025-06-22 12:59:33 -0500

    erfg

--- bronze/2020/jp3/main.cpp
+++ bronze/2020/jp3/main.cpp
@@ -3,4 +3,5 @@
 int main() {
   // PLAN: The "safe zone" is the closed interval [K, K + X - 1]. Find the minimum number of steps needed to land in the safe zone.
   // The graph of the velocity should be like a bell-shape. It should have a peak and stuff
+  // /
 }
